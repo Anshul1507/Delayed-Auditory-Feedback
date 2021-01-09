828ec0db5bee48db61433b81fa4d5ed3ca4c7066
Anshul1507 <ag1507anshul@gmail.com>
2021-01-09 23:57:15 +0530

    Added: Check Permission onStart

--- app/src/main/java/tech/anshul1507/dafapp/MainActivity.kt
+++ app/src/main/java/tech/anshul1507/dafapp/MainActivity.kt
@@ -1,11 +1,62 @@
 package tech.anshul1507.dafapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import tech.anshul1507.dafapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val REQUEST_CODE_ASK_PERMISSIONS = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*
+        * Initial Permissions
+        * */
+        requestPermission(Manifest.permission.RECORD_AUDIO)
+        requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    }
+
+    private fun requestPermission(permission: String) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(permission),
+                REQUEST_CODE_ASK_PERMISSIONS
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_ASK_PERMISSIONS -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                // Permission Denied
+                Toast.makeText(this@MainActivity, "Permission Denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions!!, grantResults)
+        }
     }
 }
