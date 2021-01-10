db4eeecb4d6638a1d7bb9338df9d35b1474dc986
Anshul1507 <ag1507anshul@gmail.com>
2021-01-10 15:51:28 +0530

    Added: UI OnClickListeners

--- app/src/main/java/tech/anshul1507/dafapp/MainActivity.kt
+++ app/src/main/java/tech/anshul1507/dafapp/MainActivity.kt
@@ -2,6 +2,8 @@ package tech.anshul1507.dafapp
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.AudioRecord
+import android.media.AudioTrack
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +18,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val REQUEST_CODE_ASK_PERMISSIONS = 101
 
+    private lateinit var audioRecord: AudioRecord
+    private lateinit var audioTrack: AudioTrack
+    private lateinit var audioData: ShortArray
+
+    private var isActive = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,6 +34,35 @@ class MainActivity : AppCompatActivity() {
         * */
         requestPermission(Manifest.permission.RECORD_AUDIO)
         requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        /*
+        * UI OnClickListeners
+        * */
+        binding.buttonStart.setOnClickListener {
+            startButtonFun()
+        }
+
+        binding.buttonStop.setOnClickListener {
+            stopButtonFun()
+        }
+
+    }
+
+    private fun startButtonFun() {
+        isActive = true
+        binding.textStatus.text = "Status: Active"
+
+        Thread { recordAndPlay() }.start()
+    }
+
+    private fun stopButtonFun() {
+        isActive = false
+        binding.textStatus.text = "Status: Inactive"
+        audioTrack.stop()
+        audioRecord.stop()
+    }
+
+    private fun recordAndPlay() {
 
     }
 
